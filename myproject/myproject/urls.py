f08582fca5e619c11263f8d23c60c61f0c37ba18
N0IC <40608736+N0IC@users.noreply.github.com>
2023-10-25 19:03:29 +0300

    Delete myproject directory

--- myproject/myproject/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from myapp import views
-
-urlpatterns = [
-    path('page1/', views.page1_view, name='page1'),
-    path('page2/', views.page2_view, name='page2'),
-    path('page3/', views.page3_view, name='page3'),
-]
